31d0fd1f69f9120771129e0fb756192563819b87
edgarjohnbi <157351953+edgarjohnbi@users.noreply.github.com>
2026-02-08 15:38:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -79,8 +79,8 @@ const CONFIG = {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/dx42hxhxt/video/upload/v1770535904/la_vie_en_rose_udhcs5.mp3", // Music streaming URL
-        startText: "🎵 Play Music",        // Button text to start music
-        stopText: "🔇 Stop Music",         // Button text to stop music
+        startText: "🎵 play music!!",        // Button text to start music
+        stopText: "🔇 stop hehe",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
     }
 };
