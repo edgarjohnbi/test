cbb81f6124f540fa6b9f02e9fb02a06d98d3e8ba
edgarjohnbi <157351953+edgarjohnbi@users.noreply.github.com>
2026-02-08 15:19:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "bebi pau, my love",
+    valentineName: "bebi pau",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -14,7 +14,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['🌝', '🌚'],  // Heart emojis
+        hearts: ['🌝', '🌚', '🌝', '🌚', '🌝', '🌚', '🌝', '🌚', '🌝', '🌚', '🌝', '🌚', '🌝', '🌚'],  // Heart emojis
         bears: ['🌛', '🌜']                       // Cute bear emojis
     },
 
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://open.spotify.com/track/3PV8uIQnxD4Qiimn3x6PVr", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
